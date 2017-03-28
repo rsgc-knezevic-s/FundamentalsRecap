ed53d9ef5f8537aff54fb4e226a54f57b1d1360d
Student <student@stefan-knezevic.local>
2017-03-28 10:27:52 -0400

    .

--- Recap.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
+++ Recap.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
@@ -24,10 +24,33 @@
 import Cocoa
 import PlaygroundSupport
 
+
+
+
 // Create canvas
 let canvas = Canvas(width: 500, height: 400)
+canvas.drawShapesWithBorders = false
 
-// Replace this comment and add your code below 
+// Replace this comment and add your code below
+var points: [NSPoint] = []
+points.append(NSPoint(x: 400, y: 100))
+points.append(NSPoint(x: 315, y: 350))
+points.append(NSPoint(x: 485, y: 350))
+
+
+canvas.fillColor = Color(hue: 120, saturation: 100, brightness: 80, alpha: 80)
+canvas.drawCustomShape(with: points)
+
+canvas.fillColor = Color(hue: 45, saturation: 100, brightness: 80, alpha: 80)
+canvas.drawRoundedRectangle(centreX: 325, centreY: 225, width: 120, height: 150, borderWidth: 1, xRadius: 20, yRadius: 20)
+
+canvas.fillColor = Color(hue: 230, saturation: 100, brightness: 80, alpha: 80)
+canvas.drawRectangle(centreX: 225, centreY: 200, width: 125, height: 350)
+
+canvas.fillColor = Color(hue: 0, saturation: 100, brightness: 80, alpha: 80)
+canvas.drawEllipse(centreX: 110, centreY: 175, width: 175, height: 175)
+
+
 
 
 /*:
